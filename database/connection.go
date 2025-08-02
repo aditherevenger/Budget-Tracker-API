cf21df3b36563fc925882687ff0f5b12caba2c4d
Aditya Yadav <adiric121@gmail.com>
2025-08-02 06:41:53 +0530

    feat: add PostgreSQL connection and migration setup (#11)
    * feat: add PostgreSQL connection and migration setup
    
    * fix dependency issue
    
    * commented linter as it support go version below 22, currently used version is 23

---
+++ database/connection.go
@@ -0,0 +1,58 @@
+package database
+
+import (
+	"fmt"
+	"github.com/aditherevenger/Budget-Tracker-API/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	//Database configuration from Environment variables
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "postgres"
+	}
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "password"
+	}
+	dbname := os.Getenv("DB_NAME")
+	if dbname == "" {
+		dbname = "budget_tracker"
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	log.Println("Database connected successfully")
+}
+
+func Migrate() {
+	err := DB.AutoMigrate(&models.User{}, &models.Category{}, &models.Transaction{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+	log.Println("Database migrated successfully")
+}
